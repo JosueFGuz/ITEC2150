9755a0ebae72c073b3c40c49ec4fdd0af0d95832
guzma <guzma@DESKTOP-09HKLS5>
2020-07-16 18:18:00 -0400

    2150HW Assignments

---
+++ Homework Assignments/ChapterTwoPtOne.java
@@ -0,0 +1,24 @@
+
+public class ChapterTwoPtOne {
+
+	public static void main(String[] args) {
+		final double Area = 250.3; 
+		
+		java.util.Scanner input=new java.util.Scanner(System.in);
+		System.out.print("Enter the width of a rectangle ");
+		double width = input.nextDouble();
+		System.out.print("Enter the length of the rectangle ");
+		double height = input.nextDouble();
+		
+		double area = width * height;
+		
+		System.out.println("The area of the rectangle is " + area);
+		
+		//Mr. Bong, with the given values for width (25.3) and height (10) , 253.0 is the area, not 250.3
+
+	}
+
+	
+		
+	}
+
---
+++ Homework Assignments/ChapterTwoPtTwo.java
@@ -0,0 +1,17 @@
+
+public class ChapterTwoPtTwo {
+
+	public static void main(String[] args) {
+
+
+	int numberOfClass = 5;
+
+	int maxNumberOfStudents = 150;
+	
+	double maxPerClass = maxNumberOfStudents / numberOfClass;
+	
+	System.out.println("The maximum amount of students per class evenly spread out is " + maxPerClass);
+	
+	}
+
+}
